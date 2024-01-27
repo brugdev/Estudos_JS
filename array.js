79cf007764722492920509fae2a985d91c7c317b
brugdev <149311124+brugdev@users.noreply.github.com>
2024-01-27 11:54:54 -0300

    Add files via upload

--- array.js
+++ array.js
@@ -1,3 +1,5 @@
+//Dado um array numérico qualquer sem valores repetidos, descubra qual é o índice do maior valor e o índice do menor valor.
+
 var array = ["88", "999", "12", "123", "230"]; 
 //armazenando max e min da lista
 var max = (Math.max(...array));
@@ -12,9 +14,8 @@ var min_str = min.toString();
 
 
 
-//verificando se virou string:
-console.log(typeof max_str);
-console.log(typeof min_str);
+//console.log(typeof max_str);
+//console.log(typeof min_str);
 
 console.log(array.indexOf(max_str));
 console.log(array.indexOf(min_str));
